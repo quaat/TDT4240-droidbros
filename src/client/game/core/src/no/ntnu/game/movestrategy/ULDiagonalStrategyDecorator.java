89f135a0ff11c084656af86a649e91a068e44fde
Thomas Hagelien <Thomas.F.Hagelien@sintef.no>
2017-04-04 12:06:27 +0200

    Added diagonal movements

--- src/client/game/core/src/no/ntnu/game/movestrategy/ULDiagonalStrategyDecorator.java
+++ src/client/game/core/src/no/ntnu/game/movestrategy/ULDiagonalStrategyDecorator.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.function.Function;
 
 import no.ntnu.game.Move;
+import no.ntnu.game.models.Board;
 import no.ntnu.game.models.Square;
 
 /**
@@ -23,6 +24,48 @@ public class ULDiagonalStrategyDecorator implements MoveStrategy {
     @Override
     public List<Function<Square, List<Move>>> legalMoves() {
         List<Function<Square, List<Move>>> moves = new ArrayList<>();
+
+        moves.add(square -> {
+            List<Move> movesList = new ArrayList<Move>();
+            Board b = square.board();
+            int col = square.col() + 1;
+            int rank = square.row() - 1;
+            // Empty squares to the lower right
+            while (col < b.cols() && rank >= 0
+                    && b.square(col, rank).piece() == null) {
+                Square dest = b.square(col, rank);
+                movesList.add(new Move(square, dest));
+                col++;
+                rank--;
+            }
+
+            // Opposite color to the lower right
+            if (col < b.cols() && rank >= 0
+                && b.square(col, rank).piece() != null
+                && b.square(col, rank).piece().color() != square.piece().color()) {
+                movesList.add(new Move(square, b.square(col, rank)));
+            }
+
+            col = square.col() - 1;
+            rank = square.col() +1 ;
+            // Empty squares to the upper left
+            while (col >= 0 && rank <= b.rows()
+                    && b.square(col, rank).piece() == null) {
+                movesList.add(new Move(square, b.square(col, rank)));
+                col--;
+                rank++;
+            }
+
+            // Opposite color to the lower right
+            if (col >= 0 && rank <= b.rows()
+                && b.square(col, rank).piece() != null
+                && b.square(col, rank).piece().color() != square.piece().color()) {
+                movesList.add(new Move(square, b.square(col, rank)));
+            }
+
+            return movesList;
+        });
+
         if (this.moveStrategy != null) {
             moves.addAll(this.moveStrategy.legalMoves());
         }
--- src/client/game/core/src/no/ntnu/game/movestrategy/URDiagonalStrategyDecorator.java
+++ src/client/game/core/src/no/ntnu/game/movestrategy/URDiagonalStrategyDecorator.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.function.Function;
 
 import no.ntnu.game.Move;
+import no.ntnu.game.models.Board;
 import no.ntnu.game.models.Square;
 
 /**
@@ -22,6 +23,49 @@ public class URDiagonalStrategyDecorator implements MoveStrategy {
     @Override
     public List<Function<Square, List<Move>>> legalMoves() {
         List<Function<Square, List<Move>>> moves = new ArrayList<>();
+
+        moves.add(square -> {
+            List<Move> movesList = new ArrayList<Move>();
+            Board b = square.board();
+            int col = square.col() + 1;
+            int rank = square.row() + 1;
+            // Empty squares to the upper right
+            while (col < b.cols() && rank < b.rows()
+                    && b.square(col, rank).piece() == null) {
+                Square dest = b.square(col, rank);
+                movesList.add(new Move(square, dest));
+                col++;
+                rank++;
+            }
+
+            // Opposite color to the upper right
+            if (col < b.cols() && rank < b.rows()
+                    && b.square(col, rank).piece() != null
+                    && b.square(col, rank).piece().color() != square.piece().color()) {
+                movesList.add(new Move(square, b.square(col, rank)));
+            }
+
+            col = square.col() - 1;
+            rank = square.row() - 1;
+
+            // Empty squares to the lower left
+            while (col >= 0 && rank >= 0
+                    && b.square(col, rank).piece() == null) {
+                movesList.add(new Move(square, b.square(col, rank)));
+                col--;
+                rank--;
+            }
+
+            // Opposite color to the lower right
+            if (col >= 0 && rank >= 0
+                    && b.square(col, rank).piece() != null
+                    && b.square(col, rank).piece().color() != square.piece().color()) {
+                movesList.add(new Move(square, b.square(col, rank)));
+            }
+
+            return movesList;
+        });
+
         if (this.moveStrategy != null) {
             moves.addAll(this.moveStrategy.legalMoves());
         }
